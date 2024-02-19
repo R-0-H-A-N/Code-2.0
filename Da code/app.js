e8d235c53fc29f15990cc88032f2abc9096af177
Rohan <rohankalia05@gmail.com>
2024-02-19 17:46:50 +0530

    Added splash screen + some minor changes

--- Da code/app.js
+++ Da code/app.js
@@ -16,3 +16,32 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+let intro = document.querySelector('.intro_splash');
+let logo = document.querySelector('.logo-header');
+let logoSpan = document.querySelectorAll('.logo');
+
+window.addEventListener('DOMContentLoaded', ()=>{
+    setTimeout(()=>{
+        
+        logoSpan.forEach((span, idx)=>{
+            setTimeout(()=>{
+                span.classList.add('active');
+            }, (idx + 1) * 400)
+        });
+
+        setTimeout(()=>{
+            logoSpan.forEach((span, idx)=>{
+                setTimeout(()=>{
+                    span.classList.remove('active')
+                    span.classList.add('fade')
+                }, (idx + 1) * 50)
+            })
+        }, 2000)
+
+        setTimeout(()=>{
+            intro.style.top = '-100vh'
+        }, 2400)
+
+    })
+})
